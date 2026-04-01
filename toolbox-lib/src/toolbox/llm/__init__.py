540a9aa1df2b2e61616ad7c177eb706a9c2aff3c
Robert Gambee <robertgambee@gmail.com>
2026-04-01 17:39:20 -0400

    Create LLMClient protocol

--- toolbox-lib/src/toolbox/llm/__init__.py
+++ toolbox-lib/src/toolbox/llm/__init__.py
@@ -1,6 +1,8 @@
+from toolbox.llm.client import LLMClient
 from toolbox.llm.data_types import Message, StructuredResult, TextResult, TokenUsage
 
 __all__ = [
+    "LLMClient",
     "Message",
     "StructuredResult",
     "TextResult",
